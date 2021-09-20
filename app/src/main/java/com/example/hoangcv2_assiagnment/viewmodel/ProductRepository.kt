5c85bd20cf3c6e027ba612819d42f742022f6337
caoviethoang999 <84014180+caoviethoang999@users.noreply.github.com>
2021-09-20 20:35:59 +0700

    Update ProductRepository.kt

--- app/src/main/java/com/example/hoangcv2_assiagnment/viewmodel/ProductRepository.kt
+++ app/src/main/java/com/example/hoangcv2_assiagnment/viewmodel/ProductRepository.kt
@@ -7,6 +7,5 @@ class ProductRepository constructor(private val productService: ProductService) 
     fun getAllProduct() = productService.getProduct()
     fun getCategory() = productService.getCategory()
     fun getProductByCategory(categoryId:Int) = productService.getProductByCategory(categoryId)
-    fun checkCategory(categoryId:Int) = productService.checkCategory(categoryId)
 
 }
